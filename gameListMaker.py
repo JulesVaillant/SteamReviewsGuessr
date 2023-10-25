49fcebba91fd85aebb9f332783f92813e3f6c706
Khopylium <atomium.boy@gmail.com>
2023-10-25 11:45:17 +0200

    gamelist maker script added

---
+++ gameListMaker.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+filename = "gameList.json"
+url = "https://steamspy.com/api.php?request=top100forever"
+response = requests.get(url)
+response_dict = response.json()
+
+with open(filename, "w", encoding="utf-8") as f:
+    index_dict = 0
+    f.write("[")
+    for key in response_dict.keys():
+        gameName = response_dict[key]["name"]
+        gameID = response_dict[key]["appid"]
+        game_dict = {
+            "name": gameName,
+            "appid":gameID
+        }
+        f.write(json.dumps(game_dict, indent=4))
+        if(index_dict != len(response_dict.keys())-1):
+            f.write(",")
+        f.write("\n")
+        index_dict+=1
+    f.write("]")
