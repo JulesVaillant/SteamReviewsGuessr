824cdf543dfcfff1db02de548ad73be7a5af5cc8
Khopylium <atomium.boy@gmail.com>
2023-10-26 13:07:54 +0200

    Error fetching reviews added

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const fs = require('fs')
 const obj = JSON.parse(fs.readFileSync('gameList.json', 'utf8'));
 const request = require('request')
 
-key = Math.floor(Math.random()*(Object.keys(obj).length-1)).toString()   //key = random [0; gameList.length]
-console.log(obj[key]['name'])
+let key = Math.floor(Math.random()*(Object.keys(obj).length-1)).toString()   //key = random [0; gameList.length]
 
 let url = "https://store.steampowered.com/appreviews/" + obj[key]['appid'] + "?json=1"
+//let url = "https://store.steampowered.com/appreviews/1046930?json=1"  //ERROR 
 let options = {json: true};
 
 request(url, options, (error, res, body) => {
@@ -15,13 +15,15 @@ request(url, options, (error, res, body) => {
 
     if (!error && res.statusCode == 200) {
         commentIndex = Math.floor(Math.random()*(res.body['reviews'].length-1)) //index = random [0 ; reviewsList.length]
-        console.log(res.body['reviews'][commentIndex]['review'])
 
-        if(res.body['reviews'][commentIndex]['voted_up']){
-            console.log("LIKE")
+        if(res.body['query_summary']['num_reviews'] != 0){
+            console.log(obj[key]['name'])
+            console.log(res.body['reviews'][commentIndex]['review'])
+            if(res.body['reviews'][commentIndex]['voted_up']) console.log("LIKE")
+            else console.log("DISLIKE")   
         }
         else{
-            console.log("DISLIKE")
+            console.log("ERROR, NO REVIEWS FOUND FOR " + obj[key]['name'])
         }
     };
 });
